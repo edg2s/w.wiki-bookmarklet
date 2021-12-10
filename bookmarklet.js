47c527c61ca22a84a6d14f79c0f707e651236407
Ed Sanders <ejsanders@gmail.com>
2021-12-10 17:45:49 +0000

    Reduce size of minified output

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,24 +1,31 @@
 ( function () {
+	// Create local variables to help the minimiser
+	var domain = 'https://meta.wikimedia.org/',
+		loc = location,
+		href = loc.href,
+		shortenurl = 'shortenurl';
+
 	function shorturlredir() {
-		location.href = 'https://meta.wikimedia.org/wiki/Special:UrlShortener?url=' + encodeURIComponent( location.href );
+		loc.href = domain + 'wiki/Special:UrlShortener?url=' + encodeURIComponent( href );
 	}
-	if ( window.mw && mw.loader ) {
+
+	try {
+		// Rather than check if mw/mw.loader exists, just use a try block (saves bytes)
 		mw.loader.using( [ 'oojs-ui-windows', 'mediawiki.widgets', 'mediawiki.ForeignApi' ] ).then( function () {
-			( new mw.ForeignApi( 'https://meta.wikimedia.org/w/api.php' ) ).post( {
-				action: 'shortenurl',
-				url: location.href
+			( new mw.ForeignApi( domain + 'w/api.php' ) ).post( {
+				action: shortenurl,
+				url: href
 			} ).then( function ( data ) {
 				var copyLayout = new mw.widgets.CopyTextLayout( {
-					copyText: data.shortenurl.shorturl
+					copyText: data[ shortenurl ].shorturl
 				} );
 				OO.ui.alert( copyLayout.$element );
 				// HACK: Wait for setup and ready processes to complete
-				setTimeout( function () {
-					copyLayout.button.focus();
-				}, 500 );
+				setTimeout( copyLayout.button.focus.bind( copyLayout.button ), 500 );
 			}, shorturlredir );
 		}, shorturlredir );
-	} else {
+	} catch ( e ) {
 		shorturlredir();
 	}
+
 }() );
