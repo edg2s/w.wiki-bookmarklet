566d45baa33db07ad73e75f030cb1dde5fd76d56
Ed Sanders <ejsanders@gmail.com>
2022-05-24 15:36:46 +0100

    Show URL length in build script

--- build/index.js
+++ build/index.js
@@ -6,4 +6,9 @@ const code = fs.readFileSync( 'src/bookmarklet.min.js' ).toString().slice( 1, -1
 const template = fs.readFileSync( 'src/index.html.template' ).toString();
 const output = template.replace( '__BOOKMARKLET__', code );
 
+// eslint-disable-next-line no-script-url
+const length = 'javascript:void();'.length + code.length;
+
+console.log( 'URL length: ' + length + ' bytes' );
+
 fs.writeFileSync( 'index.html', output );
