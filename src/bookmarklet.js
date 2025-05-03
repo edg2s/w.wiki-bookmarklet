4dcfd5c0e4f8de3fd45f59414d4bb0900560885c
Ed Sanders <ejsanders@gmail.com>
2025-05-03 13:27:49 +0300

    Add QR code

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -9,15 +9,36 @@
 	try {
 		// Rather than check if mw/mw.loader exists, just use a try block (saves bytes)
 		mw.loader.using( [ 'oojs-ui-windows', 'mediawiki.widgets', 'mediawiki.ForeignApi' ] ).then( () => {
-			( new mw.ForeignApi( domain + 'w/api.php' ) ).post( {
+			const api = new mw.ForeignApi( domain + 'w/api.php' );
+			const params = {
 				action: shortenurl,
 				// eslint-disable-next-line no-implicit-coercion
 				url: loc + ''
-			} ).then( ( data ) => {
+			};
+			Promise.all( [
+				api.post( params ),
+				api.post( {
+					...params,
+					qrcode: 1
+				} )
+			] ).then( ( data ) => {
 				const copyLayout = new mw.widgets.CopyTextLayout( {
-					copyText: data[ shortenurl ].shorturl
+					copyText: data[ 0 ][ shortenurl ].shorturl
 				} );
-				OO.ui.alert( copyLayout.$element );
+				const encodedSvg = encodeURIComponent( data[ 1 ][ shortenurl ].qrcode ).replace( /'/g, '%27' ).replace( /"/g, '%22' );
+				const dataUrl = `data:image/svg+xml;charset=utf-8,${ encodedSvg }`;
+				copyLayout.$element.append(
+					$( '<a>' ).css( { textAlign: 'center', display: 'block' } )
+						.append(
+							$( '<img>' ).attr( 'src', dataUrl )
+						)
+						.attr( {
+							href: dataUrl,
+							title: 'Download'
+							download: 'QRCode.svg'
+						} )
+				);
+				OO.ui.alert( copyLayout.$element, { size: 'medium' } );
 				// HACK: Wait for setup and ready processes to complete
 				setTimeout( copyLayout.button.focus.bind( copyLayout.button ), 500 );
 			}, shorturlredir );
